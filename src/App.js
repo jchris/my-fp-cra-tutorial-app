e11773e8406c052201267783d6a6c48f76fec765
Chris Anderson <jchris@gmail.com>
2023-09-14 16:43:54 -0700

    working todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg'
+import './App.css'
+import { useFireproof } from 'use-fireproof'
+import { fireproof, index } from '@fireproof/core'
 
 function App() {
+  const { useLiveQuery, useDocument, database } = useFireproof(fireproof('my-db'))
+  const response = useLiveQuery(index(database, 'date'))
+  const todos = response.docs
+
+  const [newTodo, setNewTodo, saveNewTodo] = useDocument({date : Date.now(), text: '', completed: false})
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={newTodo.text} onChange={(event)=>{setNewTodo({text:event.target.value})}}></input>
+      <button onClick={() => saveNewTodo().then(setNewTodo(false))}>Add</button>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo._id}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => database.put({ ...todo, completed: !todo.completed })}
+            />
+            {todo.text}
+          </li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
